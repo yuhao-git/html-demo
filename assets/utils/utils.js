002fecd6eae7ada26975c5d6b8c832d018d4b621
yuhao-git <1365940002@qq.com>
2022-05-16 11:08:09 +0800

    添加树的遍历方法

--- assets/utils/utils.js
+++ assets/utils/utils.js
@@ -4,7 +4,7 @@
  * @param {*} code 
  * @returns 
  */
- function getCodeChar(index, code = "a") {
+function getCodeChar(index, code = "a") {
     if (!index || index < 1) {
         return ""
     }
@@ -18,6 +18,87 @@
     radix26.split("").forEach(item => {
         result += String.fromCharCode(parseInt(item, 26) + baseIndex - 1)
     })
+    return result
+}
+
+/**
+ * 树遍历来源
+ * https://blog.csdn.net/weixin_42069147/article/details/115534398
+ */
+/**
+ * 深度优先遍历
+ * @param {*} tree 
+ * @param {*} func 
+ * @param {*} times 
+ * @returns 
+ */
+function treeFilter(tree, func, times = 0) {
+    if (times > 1) { // 限制递归次数
+        return false;
+    }
+    times++;
+    // 使用map复制一下节点，避免修改到原树
+    return tree
+        .map((node) => ({ ...node }))
+        .filter((node) => {
+            node.children = node.children && treeFilter(node.children, func, times);
+            return func(node) || (node.children && node.children.length);
+        });
+}
+
+
+/**
+ * 广度优先遍历
+ * @param {*} tree 
+ * @param {*} func 
+ */
+function treeForeach(tree, func) {
+    let node, list = [...tree]
+    while (node = list.shift()) {
+        func(node)
+        node.children && list.push(...node.children)
+    }
+}
+
+/**
+ * 先序遍历
+ * @param {*} tree 
+ * @param {*} func 
+ */
+function treeForeach(tree, func) {
+    tree.forEach(data => {
+        func(data)
+        data.children && treeForeach(data.children, func) // 遍历子树
+    })
+}
+
+// /**
+//  * 后序遍历
+//  * @param {*} tree 
+//  * @param {*} func 
+//  */
+// function treeForeach(tree, func) {
+//     tree.forEach(data => {
+//         data.children && treeForeach(data.children, func) // 遍历子树
+//         func(data)
+//     })
+// }
+
+/**
+ * 路径 
+ * @param {*} tree 
+ * @param {*} func 
+ * @param {*} path 
+ * @param {*} result 
+ * @returns 
+ */
+function treeFindPath(tree, func, path = [], result = []) {
+    for (const data of tree) {
+        path.push(data.id)
+        func(data) && result.push([...path])
+        data.children && treeFindPath(data.children, func, path, result)
+        path.pop()
+    }
     return result
 }
 
