4e9ea2e4226b0c762f236e9d792ecef46db3a896
yuhao-git <1365940002@qq.com>
2022-10-31 17:26:51 +0800

    新建博客项目

---
+++ blog/blog-manage/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    点击次数 <button @click="count +=1 ">{{count}}</button>
+  </div>
+</template>
+
