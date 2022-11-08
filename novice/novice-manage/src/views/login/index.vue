c0014e773660306e9e38a54d19949d7af1dfaf9f
yuhao-git <45466643+yuhao-git@users.noreply.github.com>
2022-11-09 00:10:01 +0800

    登录页

--- novice/novice-manage/src/views/login/index.vue
+++ novice/novice-manage/src/views/login/index.vue
@@ -3,7 +3,8 @@ import { ref, reactive, toRefs, onBeforeMount, onMounted, watchEffect, computed 
 import { useRoute, useRouter } from 'vue-router';
 import sun from '@/assets/svg/sun.svg'
 import moon from '@/assets/svg/moon.svg'
-
+import { User, Lock } from '@element-plus/icons-vue'
+import think from '@/assets/svg/think.svg'
 const router = useRouter()
 
 let theme = ref(true)
@@ -16,35 +17,51 @@ function changeTheme(value: boolean) {
   }
 }
 
+let user = reactive({ username: "", password: "" })
+
+function login(): void {
+  router.push("/404")
+}
 </script>
 
 <template>
-  <div class="login-container">
+  <div class="login-container pl-10 pr-10 h-full">
     <header class="navbar" > 
         <el-switch inline-prompt  v-model="theme" :active-icon="sun" :inactive-icon="moon"  @change="changeTheme"/>
     </header>
-    <div class="flex justify-center items-center">
-      <div class="flex-grow justify-center border-4 border-light-blue-500">左侧图片</div>
-      <div class="form-wrapper justify-center border-4 border-red-500">表单</div>
+
+    <div class="h-full grid grid-cols-2  gap-40 justify-center items-center">
+      <div class="form-wrapper w-20 p-10 col-start-2">
+        <h1 class="mb-1">NOVICE</h1>
+        <p class="text-xs mb-6 text-gray-500">后台管理系统</p>
+        <el-form>
+          <el-form-item >
+            <el-input class="shadow-md h-10 border-0" v-model="user.username" :prefix-icon="User" placeholder="账号" />
+          </el-form-item>
+          <el-form-item >
+            <el-input class="shadow-md h-10 border-0" v-model="user.password" :prefix-icon="Lock" placeholder="密码" />
+          </el-form-item>
+          <el-button  type="primary" class="w-full shadow-lg" @click="login"> 登录 </el-button>
+        </el-form>
+      </div>
     </div>
  </div>
 </template>
 
 <style scoped lang='less'>
-.login-container{
-  height: 100%;
+.login-container {
+  background: url('@/assets/svg/think.svg') no-repeat 20% center / 900px 700px;
+
   .navbar {
-  position: absolute;
-  right: 16px;
-  top: 10px;
-}
+    position: absolute;
+    right: 16px;
+    top: 10px;
+  }
 }
 
-.flex{
-  height: 100%;
-  .form-wrapper{
-    width: 500px;
-  }
+.form-wrapper {
+  width: 420px;
+
 }
 </style>
 
